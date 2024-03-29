fee66dcc725fa6cb729ff7fe940bd25671d31593
Alex-stree <145285448+Alex-stree@users.noreply.github.com>
2024-03-29 15:20:47 +0500

    project
    project

--- Фролова_Итоговое_задание_модуль_4/src/routes/ticher/+page.svelte
+++ Фролова_Итоговое_задание_модуль_4/src/routes/ticher/+page.svelte
@@ -1,27 +1,34 @@
-<body>
-    <div class="container">
-      <h1>Учителя</h1>
-      <div id="rasList">
-        <div class="ras">
-          <h2>Зайнетдинов Данил Радикович</h2>
-          <p>Преподаватель</p>
-        </div>
-        <div class="ras">
-          <h2>Бирюкова Юлия Николаевна</h2>
-          <p>Преподаватель</p>
-        </div>
-        <div class="ras">
-          <h2>Бирюков Андрей Викторович</h2>
-          <p>Преподаватель</p>
-        </div>
-        <div class="ras">
-          <h2>Нургалиева Венера Раилевна</h2>
-          <p>Преподаватель</p>
-        </div>
-        <div class="ras">
-          <h2>Милованов Петр Николаевич</h2>
-          <p>Преподаватель</p>
-        </div>
+<div class="container">
+      <h1>Фильм порядок показа</h1>
+      <button id="addButton">Фильм открыть</button>
+      <div id="filmsList">
       </div>
-    </div>
-  </body>
+      <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        let films = [
+        { title: 'Онегин', ganr: 'Мелодрама' },
+        { title: 'Материнский инстинкт', ganr: 'Психологический триллер' },
+        { title: 'Уиджи. Потустороннее измерение', ganr: 'Хоррор' },
+        { title: 'Ненормальный', ganr: 'Драма, комедия' },
+        { title: 'Омен. Непорочная', ganr: 'Хоррор' }];
+        let addButton = document.getElementById('addButton');
+        let filmsList = document.getElementById('filmsList');
+        
+        addButton.addEventListener('click', function() {
+        let film = films[Math.floor(Math.random() * 10)];
+        let filmElement = document.createElement('div');
+        filmElement.classList.add('film');
+        
+        let titleElement = document.createElement('h2');
+        titleElement.textContent = film.title;
+        filmElement.appendChild(titleElement);
+        
+        let ganrElement = document.createElement('p');
+        ganrElement.textContent = 'Жанр: ' + film.ganr;
+        filmElement.appendChild(ganrElement);
+        
+        filmsList.appendChild(filmElement);
+        });
+        });     
+      </script>   
+</div>
